5d69856f34f60940e877f90905cd8e8f6ccae957
Kate Miholenko <katemixa92@gmail.com>
2022-06-20 22:36:22 +0300

    refactor: added class Calculator, object creation is implemented in main

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,16 +1,13 @@
-import java.util.Scanner;
-
 public class Calculator {
-    public static void main(String[] args) {
-        Scanner get = new Scanner(System.in);
-        System.out.println("Введите первое число");
-        int a = get.nextInt();
-        System.out.println("Введите второе число");
-        int b = get.nextInt();
-        int sum = a + b;
-        int multiply = a * b;
-        System.out.println("Сумма равна " + sum);
-        System.out.println("Произведение равно " + multiply);
+    public Calculator() {
+    }
+
+    public static void printSumResult(int firstNum, int secondNum) {
+        System.out.println(firstNum + " + " + secondNum + " = " + (firstNum + secondNum));
+    }
+
+    public static void printMultiplyResult(int firstNum, int secondNum) {
+        System.out.println(firstNum + " * " + secondNum + " = " + (firstNum * secondNum));
     }
 
 }
